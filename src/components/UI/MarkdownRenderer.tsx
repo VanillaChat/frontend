cf5ab95c90f9017a05bc66885c91414996a9a44a
ciach0_ <ciach795@gmail.com>
2025-08-02 19:52:14 +0200

    0.0.13B-1: Better colored text

--- src/components/UI/MarkdownRenderer.tsx
+++ src/components/UI/MarkdownRenderer.tsx
@@ -124,27 +124,27 @@ export const MarkdownRenderer: FC<SimpleMarkdownProps> = ({ children }) => {
     }[] = [
         {
             regex: /~~(.+?)~~/,
-            render: ([, content]) => <del key={nanoid()}>{parseInline(content)}</del>,
+            render: ([, content], ctx) => <del key={nanoid()}>{parseInline(content, ctx)}</del>,
         },
         {
             regex: /\*\*\*(.+?)\*\*\*/,
-            render: ([, content]) => <strong key={nanoid()}><em>{parseInline(content)}</em></strong>,
+            render: ([, content], ctx) => <strong key={nanoid()}><em>{parseInline(content, ctx)}</em></strong>,
         },
         {
             regex: /_\*\*(.+?)\*\*_?/,
-            render: ([, content]) => <strong key={nanoid()}><em>{parseInline(content)}</em></strong>,
+            render: ([, content], ctx) => <strong key={nanoid()}><em>{parseInline(content, ctx)}</em></strong>,
         },
         {
             regex: /\*\*_([^_]+)_\*\*/,
-            render: ([, content]) => <strong key={nanoid()}><em>{parseInline(content)}</em></strong>,
+            render: ([, content], ctx) => <strong key={nanoid()}><em>{parseInline(content, ctx)}</em></strong>,
         },
         {
             regex: /\*\*(.+?)\*\*/,
-            render: ([, content]) => <strong key={nanoid()}>{parseInline(content)}</strong>,
+            render: ([, content], ctx) => <strong key={nanoid()}>{parseInline(content, ctx)}</strong>,
         },
         {
             regex: /(?<!\w)_(?!_)(.+?)(?<!_)_(?!\w)|(?<!\w)\*(?!\*)(.+?)(?<!\*)\*(?!\w)/,
-            render: (m) => <em key={nanoid()}>{parseInline(m[1] || m[2])}</em>,
+            render: (m, ctx) => <em key={nanoid()}>{parseInline(m[1] || m[2], ctx)}</em>,
         },
         { regex: /`([^`]+)`/, render: ([, code]) => <code key={nanoid()} className="px-1 py-0.5 rounded bg-muted font-mono text-sm">{code}</code> },
         {
